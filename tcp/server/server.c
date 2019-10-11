512c4eeb0c6604f0e779c973ea9b0b373f4a4ace
Nhung Bui <buithuynhung1997@gmail.com>
2019-10-12 00:11:51 +0300

    Done Chat

---
+++ tcp/server/server.c
@@ -0,0 +1,254 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+
+#define DEF_PORT 8888
+#define DEF_IP "127.0.0.1"
+#define how_shutdown 2 //зарпещена передача данных
+#define msg_length 256
+#define time_length 50
+#define name_length 40
+#define total_length (msg_length + time_length + name_length + sizeof("<>:\r\n"))
+#define max_clients 50
+#define EXIT "/exit"
+
+int clients_sockets[max_clients];
+pthread_t clients_threads[max_clients];
+int clients_numb = 0, ret;
+pthread_mutex_t mp;
+pthread_mutexattr_t matr;
+
+int readN(int sock, char* buf, int length, int flags);
+int readFix(int sock, char* buf, int bufSize, int flags);
+int sendFix(int sock, char* buf, int flags);
+
+// обработка одного клиента
+void* clientHandler(void* arg)
+{
+	int pos = (int) arg;
+    int sock = clients_sockets[pos];
+    char msg_buf[msg_length];
+    char time_buf[time_length];
+	char text[total_length];
+    int res = 0;
+    time_t curtime;
+    struct tm* loc_time;
+
+    for (;;) {
+        bzero(msg_buf, msg_length);
+		bzero(time_buf, time_length);
+		bzero(text, total_length);
+
+        res = readFix(sock, msg_buf, msg_length, 0);
+        if (res <= 0) {
+            perror("\nA client left chat\n");
+
+			pthread_mutex_lock(&mp);
+			clients_sockets[pos] = 0;
+			pthread_mutex_unlock(&mp);
+            
+			shutdown(sock, how_shutdown);
+            close(sock);
+            pthread_exit(NULL);
+        }
+
+        curtime = time(NULL);
+        loc_time = localtime(&curtime);
+        strftime(time_buf, time_length, "<%I:%M %p> ", loc_time);
+	
+		strcpy(text, time_buf);
+        strcat(text, msg_buf);
+
+        printf("%s\n", text);
+		
+		pthread_mutex_lock(&mp);
+		for(int i = 0; i < clients_numb; i++) {
+			if (clients_sockets[i] != 0) {
+				res = sendFix(clients_sockets[i], text, 0);
+				if (res <= 0) {
+					perror("send call failed");
+					shutdown(sock, how_shutdown);
+					close(sock);
+					pthread_exit(NULL);
+				}
+			}
+        }
+        pthread_mutex_unlock(&mp);
+
+    }
+}
+
+void* acceptHandler(void* arg)
+{
+	int server = (int) arg;
+    struct sockaddr_in cli_addr;
+	unsigned int clilen = sizeof(cli_addr);
+	int i = 0;
+	int res;
+	for (;;) {
+
+        clients_sockets[i] = accept(server, (struct sockaddr*) &cli_addr, &clilen);
+		if (clients_sockets[i] < 0) {
+			perror("ERROR on accept");
+			exit(1);
+		}
+		
+        puts("\nNew client connected!\n");
+
+        pthread_mutex_lock(&mp);
+		clients_numb = i + 1;
+        pthread_mutex_unlock(&mp);
+
+        res = pthread_create(&clients_threads[i], NULL, clientHandler, (void*)(i));
+        if (res) {
+            printf("Error while creating new thread\n");
+        }
+		
+		i++;
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+    int port = 0;
+    if (argc < 2) {
+        printf("Using default port %d\n", DEF_PORT);
+        port = DEF_PORT;
+    }
+    else
+        port = atoi(argv[1]);
+
+	printf ("Enter <%s> to quit\n", EXIT);
+	
+	ret = pthread_mutexattr_init(&matr);
+    ret = pthread_mutexattr_setpshared(&matr, PTHREAD_PROCESS_PRIVATE);
+    pthread_mutex_init(&mp,&matr);
+	
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server < 0) {
+        perror("Can't create socket to listen: ");
+        exit(1);
+    }
+
+    if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) {
+        perror("ERROR on setsockopt");
+        shutdown(server, how_shutdown);
+        close(server);
+    }
+
+    int res = bind(server, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (res < 0) {
+        perror("Can't bind socket");
+        shutdown(server, how_shutdown);
+        close(server);
+        exit(1);
+    }
+    // слушаем входящие соединения
+    res = listen(server, max_clients);
+    if (res) {
+        perror("Error while listening:");
+        exit(1);
+    }
+    puts("Waiting for incoming connections");
+    // основной цикл работы
+	
+	pthread_t tid;
+	res = pthread_create(&tid, NULL, acceptHandler, (void*) server);
+	
+	for (;;) {
+		char text[msg_length];
+		fgets(text, msg_length, stdin);
+		text[strlen(text) - 1] = '\0';
+		
+		if (!strCmp(text, EXIT)) {	
+			printf("Goodbye!\n");
+			pthread_cancel(tid);
+			strcpy(text, "/goodbye");
+			
+	        pthread_mutex_lock(&mp);
+			for (int i = 0; i < clients_numb; i++) {
+				if (clients_sockets[i] != 0){
+					sendFix(clients_sockets[i], text, 0);
+					close(clients_sockets[i]);
+					pthread_cancel(clients_threads[i]);
+				}
+			}
+	        pthread_mutex_unlock(&mp);
+
+			close(server);
+			exit(1);
+		}
+		printf ("Illegal input\n");
+	}
+	
+    shutdown(server, how_shutdown);
+    close(server);
+    return 0;
+}
+
+int strCmp(char* static_str, char* final_str) {
+	int count = 0;
+	for (int i = 0; static_str[i]; i++)
+		if (static_str[i] != ' ')
+			static_str[count++] = static_str[i];
+	
+	static_str[count] = '\0';
+	
+	if (!strncasecmp(static_str, final_str, strlen(final_str)) 
+		&& strlen(static_str) == strlen(final_str)) return 0;
+	else return 1;
+}
+
+int readN(int sock, char* buf, int length, int flags) {
+	int left;
+	int read;
+	char* ptr;
+	
+	ptr = buf;
+	left = length;
+	while (left > 0) {
+		if ((read = recv(sock, ptr, left, flags)) <= 0) {
+			return -1;
+		}
+		left -= read;
+		ptr += read;
+	}
+	return (length - left);
+}
+
+int readFix(int sock, char* buf, int bufSize, int flags)
+{
+    // читаем "заголовок" - сколько байт составляет наше сообщение
+    unsigned msgLength = 0;
+    int res = readN(sock, &msgLength, sizeof(unsigned), flags | MSG_WAITALL);
+    if (res <= 0)
+        return res;
+    if (res > bufSize) {
+        printf("Recieved more data, then we can store, exiting\n");
+        exit(1);
+    }
+    // читаем само сообщение
+    return readN(sock, buf, msgLength, flags | MSG_WAITALL);
+}
+
+int sendFix(int sock, char* buf, int flags)
+{
+    // шлем число байт в сообщении
+    unsigned msgLength = strlen(buf);
+    int res = send(sock, &msgLength, sizeof(unsigned), flags);
+    if (res <= 0)
+        return res;
+    send(sock, buf, msgLength, flags);
+}
