c0c171a26dbedb7ce47e5abafb39f167b3b6f636
DanilaEremenko <danilaeremenko@mail.ru>
2019-11-21 12:10:53 +0300

    tftp:server, client: arg parsing added intro runners

--- tftp/run_client.py
+++ tftp/run_client.py
@@ -1,7 +1,31 @@
+import argparse
 from tftp_test import Client
 
-tftpClient = Client("localhost", None, None)
-## get
-tftpClient.get()
-## put
-tftpClient.put()
+if __name__ == '__main__':
+
+    # ---------------------------- parsing arguments ----------------------------
+    parser = argparse.ArgumentParser(description="Some description")
+
+    parser.add_argument("-p", "--port", type=int, action='store',
+                        help="port")
+
+    parser.add_argument("-i", "--ip", type=str, action='store',
+                        help="ip")
+
+    args = parser.parse_args()
+
+    serverPort = args.port
+
+    serverIp = args.ip
+
+    if serverPort is None:
+        raise Exception("-p Port was't passed")
+    if serverIp is None:
+        raise Exception("-i IP was't passed")
+
+    # ---------------------------- running client ----------------------------
+    tftpClient = Client(serverIp, serverPort, "client_dir", "Mikrokontrollery.pdf")
+    ## get
+    tftpClient.get()
+    ## put
+    # tftpClient.put()
--- tftp/run_server.py
+++ tftp/run_server.py
@@ -1,6 +1,29 @@
+import argparse
 import os
 from tftp_test import Server
 
-## server running
-tftpServer = Server(os.path.abspath('data_dir'), 5000)
-tftpServer.run()
+if __name__ == '__main__':
+
+    # ---------------------------- parsing arguments ----------------------------
+    parser = argparse.ArgumentParser(description="Some description")
+
+    parser.add_argument("-p", "--port", type=int, action='store',
+                        help="port")
+
+    parser.add_argument("-d", "--dir", type=str, action='store',
+                        help="direcotry with data")
+
+    args = parser.parse_args()
+
+    localDir = args.dir
+
+    localPort = args.port
+
+    if localDir is None:
+        raise Exception("-d Dir was't passed")
+    if localPort is None:
+        raise Exception("-p Port was't passed")
+
+    # ---------------------------- running server ----------------------------
+    tftpServer = Server(os.path.abspath('server_dir'), localPort)
+    tftpServer.run()
