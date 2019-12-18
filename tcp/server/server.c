2ed77f52b60335cfb84d3f3c469749b4a9987e50
Nhung Bui <buithuynhung1997@gmail.com>
2019-12-18 22:59:38 +0300

    Fix formatting

--- tcp/server/server.c
+++ tcp/server/server.c
@@ -38,72 +38,70 @@ int poll_size, server;
 
 int main(int argc, char** argv)
 {
-
-    int port = 0;
-    if (argc < 2) 
+	int port = 0;
+	if (argc < 2) 
 	{
-        printf("Using default port %d\n", DEF_PORT);
-        port = DEF_PORT;
-    }
-    else
-        port = atoi(argv[1]);
-
+		printf("Using default port %d\n", DEF_PORT);
+		port = DEF_PORT;
+	}
+	else
+		port = atoi(argv[1]);
+	
 	if (signal(SIGINT, close_server) == SIG_ERR) {
-        perror("ERROR on sigint_handler");
-        exit(1);
-    }
+		perror("ERROR on sigint_handler");
+		exit(1);
+	}
 	
 	struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
-    int server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server < 0) 
+	int server = socket(AF_INET, SOCK_STREAM, 0);
+	if (server < 0) 
 	{
-        perror("Can't create socket to listen: ");
-        exit(1);
-    }
-
-    if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) 
-	{
-        perror("ERROR on setsockopt");
-        shutdown(server, how_shutdown);
-        close(server);
+		perror("Can't create socket to listen: ");
 		exit(1);
-    }
+	}
+	
+	if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) 
+	{
+		perror("ERROR on setsockopt");
+		shutdown(server, how_shutdown);
+		close(server);
+		exit(1);
+	}
 	
 	if (fcntl(server, F_SETFL, O_NONBLOCK) < 0) 
 	{
-        perror("ERROR on making socket nonblock:");
-        close(server);
+		perror("ERROR on making socket nonblock:");
+		close(server);
 		exit(1);
-    }
-
-    int res = bind(server, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    if (res < 0)
+	}
+	
+	int res = bind(server, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if (res < 0)
 	{
-        perror("Can't bind socket");
-        shutdown(server, how_shutdown);
-        close(server);
-        exit(1);
-    }
-    // слушаем входящие соединения
-    res = listen(server, max_clients);
-    if (res) 
+		perror("Can't bind socket");
+		shutdown(server, how_shutdown);
+		close(server);
+		exit(1);
+	}
+	// слушаем входящие соединения
+	res = listen(server, max_clients);
+	if (res) 
 	{
-        perror("Error while listening:");
-        exit(1);
-    }
+		perror("Error while listening:");
+		exit(1);
+	}
 	
-	 puts("Waiting for incoming connections");
+	puts("Waiting for incoming connections");
 	
-	//initiate pollfd
-	
+	//initiate pollfd	
 	fds[0].fd = server;
 	fds[0].events = POLLIN;
 	poll_size = 1;
-
+	
 	for (int i = 1; i < max_fds; i++) 
 			fds[i].fd = -1;
 	
@@ -142,7 +140,7 @@ int main(int argc, char** argv)
 								}
 								break;
 							}
-
+	
 							poll_size++;
 							fds[poll_size - 1].fd = cli_sock;
 							fds[poll_size - 1].events = POLLIN;
@@ -173,20 +171,20 @@ int main(int argc, char** argv)
 	}
 	
 	close_server();
-    return 0;
+	return 0;
 }
 
 void communicate_with_client (int fd) {
 	char msg_buf[msg_length];
-    char time_buf[time_length];
+	char time_buf[time_length];
 	char text[total_length];
-    time_t curtime;
-    struct tm* loc_time;
+	time_t curtime;
+	struct tm* loc_time;
 	
 	bzero(msg_buf, msg_length);
 	bzero(time_buf, time_length);
 	bzero(text, total_length);
-
+	
 	int res = readFix(fd, msg_buf, msg_length, 0);
 	
 	if (res <= 0) 
@@ -291,25 +289,25 @@ int readN(int sock, char* buf, int length, int flags)
 
 int readFix(int sock, char* buf, int bufSize, int flags)
 {
-    // читаем "заголовок" - сколько байт составляет наше сообщение
-    unsigned msgLength = 0;
-    int res = readN(sock, &msgLength, sizeof(unsigned), flags | MSG_WAITALL);
-    if (res <= 0)
-        return res;
-    if (res > bufSize) {
-        printf("Recieved more data, then we can store, exiting\n");
-        exit(1);
-    }
-    // читаем само сообщение
-    return readN(sock, buf, msgLength, flags | MSG_WAITALL);
+	// читаем "заголовок" - сколько байт составляет наше сообщение
+	unsigned msgLength = 0;
+	int res = readN(sock, &msgLength, sizeof(unsigned), flags | MSG_WAITALL);
+	if (res <= 0)
+		return res;
+	if (res > bufSize) {
+		printf("Recieved more data, then we can store, exiting\n");
+		exit(1);
+	}
+	// читаем само сообщение
+	return readN(sock, buf, msgLength, flags | MSG_WAITALL);
 }
 
 int sendFix(int sock, char* buf, int flags)
 {
-    // шлем число байт в сообщении
-    unsigned msgLength = strlen(buf);
-    int res = send(sock, &msgLength, sizeof(unsigned), flags);
-    if (res <= 0)
-        return res;
-    send(sock, buf, msgLength, flags);
+	// шлем число байт в сообщении
+	unsigned msgLength = strlen(buf);
+	int res = send(sock, &msgLength, sizeof(unsigned), flags);
+	if (res <= 0)
+		return res;
+	send(sock, buf, msgLength, flags);
 }
